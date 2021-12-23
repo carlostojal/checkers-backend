c34eb8155c6953c7a7b8324ddfc77ee9d44c062d
Carlos Tojal <carlos.tojal@hotmail.com>
2021-12-23 12:28:48 +0000

    create dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:current-alpine
+
+WORKDIR /usr/src/checkers-backend
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
+CMD ["npm", "start"]
--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 // Import the express in typescript file
 import express from 'express';
 import http from 'http';
+import { Server } from 'socket.io';
  
 // Initialize the express engine
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
+const io: Server = new Server(server);
  
 // Take a port 3000 for running server.
 const port: number = 3000;
@@ -12,6 +14,10 @@ const port: number = 3000;
 // Handling '/' Request
 app.get('/', (_req, _res) => {
     _res.send("TypeScript Wiht Expresss");
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected.");
 });
  
 // Server setup
